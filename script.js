7d206ef51f731092159511d9a30a57da39f9ea32
Sabarish R T <sabarishrt@gmail.com>
2025-12-22 21:35:27 +0530

    go

---
+++ script.js
@@ -0,0 +1,30 @@
+ //variables
+//  var a=100;
+//  let b=200;
+//  const c=300;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+//Number
+let a=10;
+let b=10.5;
+console.log(a);
+console.log(b);
+console.log(typeof(a));
+console.log(typeof(b));
+//String
+let cname="BIT";
+console.log(cname);
+//Boolean
+let pass=true;
+console.log(pass);
+console.log(typeof(pass));
+//null
+let data=null;
+console.log(typeof(data));
+let marks=Number(prompt(""));
+if(marks >=60 && marks <+89){
+    console.log("A");
+    else if(marks>=60 && marks<=89)
+}
